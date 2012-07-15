49db5222651ada0d27ccd48e516c4f965f99e3c6
Simon Lamb <s.lamb@hull.ac.uk>
2012-07-16 00:12:07 +0100

    Playing with Blacklight design

---
+++ tmp/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'bbc63e64956614d583609794ed492d31bb909be23f5e1179078254e4859c60f6da1585ab9150e1dd0478f5af9bf58ef792829b55134d90e08b1ae7e93d45da22'
