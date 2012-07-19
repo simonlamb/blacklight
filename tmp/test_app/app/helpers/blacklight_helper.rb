01fd22edf11d9ed002ac3e6fb0f31f1eab7d7c2e
Simon Lamb <s.lamb@hull.ac.uk>
2012-07-19 18:15:44 +0100

    More updates various pages

--- tmp/test_app/app/helpers/blacklight_helper.rb
+++ tmp/test_app/app/helpers/blacklight_helper.rb
@@ -1,9 +1,14 @@
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
- # collection of items to be rendered in the @sidebar
+ # collection of items to be rendered in the @topbar
   def topbar_items
     @topbar_items ||= []
+  end
+
+ # collection of items to be rendered in the @searchbar 
+  def search_bar
+    @search_bar ||= []
   end
 
 
